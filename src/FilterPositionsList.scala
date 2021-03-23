6e37b867b55d1e20e5686903d77036434cbafee7
devs-93 <shukla.devbrat1993@gmail.com>
2021-03-23 13:05:00 +0530

    FilterPositionsList

---
+++ src/FilterPositionsList.scala
@@ -0,0 +1,8 @@
+object FilterPositionsList extends App {
+  def f(arr: List[Int]) = {
+    val filterPositionsList = arr.zipWithIndex
+    val oddDataList = filterPositionsList.filter(x => (x._2 % 2 != 0)).map(x=>x._1)
+    oddDataList
+  }
+  println(f(List(1, 2, 3, 4, 5, 6, 7, 8, 9)))
+}
