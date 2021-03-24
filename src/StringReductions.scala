2ceacdc82aa2cd550a315c08c49e847fc99c42f1
devs-93 <shukla.devbrat1993@gmail.com>
2021-03-24 12:44:49 +0530

    StringReductions

---
+++ src/StringReductions.scala
@@ -0,0 +1,21 @@
+object StringReductions {
+  def main(args: Array[String]) {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution*/
+    val stdin = scala.io.StdIn
+    val inputData = stdin.readLine().trim()
+
+
+    def stringReductionFunction(stringToReduce: String, reducedString: String): String = {
+      if (stringToReduce.isEmpty) {
+        reducedString
+      }
+      else if (reducedString.contains(stringToReduce.head)) {
+        stringReductionFunction(stringToReduce.tail, reducedString)
+      }
+      else {
+        stringReductionFunction(stringToReduce.tail, reducedString + stringToReduce.head)
+      }
+    }
+    println(stringReductionFunction(inputData, ""))
+  }
+}
