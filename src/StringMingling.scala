c50b14d064c4e7cf73ee10ef076c5be28421d6f8
devs-93 <shukla.devbrat1993@gmail.com>
2021-03-24 12:13:56 +0530

    String Mingling

--- src/StringMingling.scala
+++ src/StringMingling.scala
@@ -5,7 +5,7 @@ object StringMingling {
     val firstString = stdin.readLine().trim()
     val secondString = stdin.readLine().trim()
     val zippedData = firstString.toList zip secondString.toList
-    val result = zippedData.flatten(x => x._1.toString.concat(x._2.toString))
+    val result = zippedData.map(x => x._1.toString.concat(x._2.toString))
     println(result.mkString(""))
   }
 }
